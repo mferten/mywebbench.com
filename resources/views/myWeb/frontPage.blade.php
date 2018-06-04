6a7ff9c2f7a2080e286ddadd84e54a64ea9a1b8b
Mehmet F Erten <mferten@mfeweb.com>
2018-06-04 08:56:19 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -5,7 +5,7 @@
             <h1>USVI St. Thomas Should Be A Cheerful Paradise</h1>
             <figure>
                 <a href="images/IMG1.JPG" target="_blank">
-                    <img id="picture" class="img28 aPng" src="images/IMG1.JPG"
+                    <img id="picture" class="img24 aPng" src="images/IMG1.JPG"
                         alt="St. Thomas Photo"/>
                 </a>
             </figure>
@@ -16,9 +16,28 @@
         <main>
             <ul class="ulListStyle">
                 <li>
-                    <span class="harvard">invite </span><a href="https://www.wholefoodsmarket.com/" target="_blank">&nbsp;Whole Foods&nbsp;</a>
-                    <span class="harvard">invite </span><a href="https://www.costco.com/" target="_blank">&nbsp;COSTCO&nbsp;</a>
-                    <span class="harvard">and </span><a href="http://www.magensbayauthority.com/" target="_blank">&nbsp;Set An Aruba Behind Megans Bay&nbsp;</a>
+                    <span class="harvard">Invite </span>
+                </li>
+                <li>
+                    <a href="https://www.wholefoodsmarket.com/" target="_blank">&nbsp;Whole Foods&nbsp;&nbsp;</a>
+                </li>
+                <li>
+                    <span class="harvard">Invite </span>
+                </li>
+                <li>
+                    <a href="https://www.costco.com/" target="_blank">&nbsp;COSTCO&nbsp;&nbsp;</a>
+                </li>
+                <li>
+                    <span class="harvard">Set </span>
+                </li>
+                <li>
+                    <a href="http://www.magensbayauthority.com/" target="_blank">&nbsp;An Aruba Behind Megans Bay&nbsp;</a>
+                </li>
+                <li>
+                    <span class="harvard">Ongoing </span>
+                </li>
+                <li>
+                    <a href="http://www.magensbayauthority.com/" target="_blank">&nbsp;Solar Energy: Roads, Buildings and Beautification&nbsp;</a>
                 </li>
             </ul>
         </main>
