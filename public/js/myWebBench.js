02652abdbb32fdd3dca20e8acd9b5de944e60851
Mehmet F Erten <mferten@mfeweb.com>
2018-06-04 10:57:12 -0400

    commit all

--- public/js/myWebBench.js
+++ public/js/myWebBench.js
@@ -10,13 +10,15 @@ this.addEventListener("load", startUp, false);
  */
 function startUp()
 {
+
+    this.addEventListener("click", clickEvents, false);
     // image count
     var currentCarouselImage = 0; // if 0 is not assigned: It becomes "undefined" and NaN in setCarouselImage function.
     // set the first Image
     setCarouselImage();
     // set next picture of this Real Estate or Resort (Carousel) every 2.5 seconds
     setInterval(setCarouselImage, 4500);
-
+    // Rotate the Photos
     function setCarouselImage()
     {
         // if the last one, loop from the first one
@@ -28,7 +30,37 @@ function startUp()
         {
             currentCarouselImage++;
         }
-        console.log("images/IMG" + currentCarouselImage + ".JPG");
         document.getElementById("picture").src="images/IMG" + currentCarouselImage + ".JPG";
     }
+}
+
+/* a Flag is Selected: The Search starts */
+function clickEvents(event)
+{
+    // Modify the Photo Size
+    if (event.target.id == "increase")
+    {
+        // "+" Increase the Size
+        changeTheSize("+");
+    }
+    // if not Increase, is it "Decrease"?
+    else if (event.target.id == "decrease")
+    {
+        // "-" Decrease the Size
+        changeTheSize("-");
+    }
+}
+
+// Adjust the Photo size
+function changeTheSize(what)
+{
+    if (document.getElementById("picture").classList.contains("img20"))
+    {
+        document.getElementById("picture").classList.remove("img20");
+        document.getElementById("picture").width = "320";
+    }
+    if (what == "+")
+        document.getElementById("picture").width = document.getElementById("picture").width + 48;
+    else
+        document.getElementById("picture").width = document.getElementById("picture").width - 48;
 }
