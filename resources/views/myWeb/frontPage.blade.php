3289f65eb2342b33bca12eae6098f74fce66c57d
Mehmet F Erten <mferten@mfeweb.com>
2018-06-14 10:32:13 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -5,11 +5,11 @@
             <h1>Let's Make USVI St. Thomas A Cheerful Paradise <a class="harvard" href="docs/whyCheerfulParadise.jpg" target="_blank">How?&nbsp;</a></h1>
             <!-- Photos -->
             <figure id="photoBox">
-                <img id="previous" class="threeRems displayNone" src="images/previous.png" alt="Previous Photo"/>
-                <img id="decrease" class="threeRems displayType" src="images/decrease.png" alt="Decrease Photo Size"/>
-                <img id="startOrStop" class="threeRems" src="images/pause.png" alt="Stop Carousel"/>
-                <img id="increase" class="threeRems displayType" src="images/increase.png" alt="Increase Photo Size"/>
-                <img id="next" class="threeRems displayNone" src="images/next.png" alt="Next Photo"/>
+                <img id="previous" class="threeRems displayNone" src="images/previous.png" alt="Previous Photo" title="Previous Photo"/>
+                <img id="decrease" class="threeRems displayType" src="images/decrease.png" alt="Decrease Photo Size" title="Decrease Photo Size"/>
+                <img id="startOrStop" class="threeRems" src="images/pause.png" alt="Stop Carousel" title="Stop Photo Carousel"/>
+                <img id="increase" class="threeRems displayType" src="images/increase.png" alt="Increase Photo Size" title="Increase Photo Size"/>
+                <img id="next" class="threeRems displayNone" src="images/next.png" alt="Next Photo" title="Next Photo"/>
                 <figcaption id="imageText" class="harvard">Where is this place and why it is special?</figcaption>
                 <a id="pictureA" href="images/IMG1.JPG" target="_blank">
                     <img id="picture" class="img20 aPng" src="images/IMG1.JPG"
@@ -18,9 +18,9 @@
             </figure>
             <!-- User Opinion -->
             <figure id="emailBox" class="displayNone">
-                <img id="sendButton" class="threeRems" src="images/sendMail.jpg" alt="Send"/>
-                <img id="clearButton" class="threeRems" src="images/clear.jpg" alt="Clear"/>
-                <img id="returnButton" class="threeRems" src="images/return.jpg" alt="Return"/>
+                <img id="sendButton" class="threeRems" src="images/sendMail.jpg" alt="Send" title="Send your eMail"/>
+                <img id="clearButton" class="threeRems" src="images/clear.jpg" alt="Clear" title="Clear your eMail"/>
+                <img id="returnButton" class="threeRems" src="images/return.jpg" alt="Return" title="Return to Main Page"/>
                 <form id="SendMailModule" method="post">
                     <label for="mailText" class='harvard'>Please Enter Your Opinion</label>
                     <textarea class="img20 aPng" id="mailText" rows="15" cols="50"></textarea>
